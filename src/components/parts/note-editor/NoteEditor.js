ccd5bfcf819684ef8f037c9193f9db438aa067ae
James Cathcart <47301496+james-cathcart@users.noreply.github.com>
2021-10-26 05:33:08 -0600

    incorporated TinyMCE react package
    Applied dark mode configuration to TinyMCE also

--- src/components/parts/note-editor/NoteEditor.js
+++ src/components/parts/note-editor/NoteEditor.js
@@ -1,16 +1,40 @@
+import './note-editor.css'
 import * as React from 'react';
-import {TextareaAutosize} from "@mui/material";
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef } from "react";
 
 const NoteEditor = () => {
 
+  const editorRef = useRef(null);
+  // const log = () => {
+  //   if (editorRef.current) {
+  //     console.log(editorRef.current.getContent());
+  //   }
+  // };
+
   return (
     <div class="editorContainer">
-      <TextareaAutosize
-        class="editorContainer"
-        aria-label="empty textarea"
-        placeholder="Empty"
-        style={{ width: 200 }}
-      />
+        <Editor
+          onInit={(evt, editor) => editorRef.current = editor}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            skin: "oxide-dark",
+            content_css: "dark",
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        {/*<button onClick={log}>Log editor content</button>*/}
     </div>
   );
 
--- src/components/parts/note-list/NoteList.js
+++ src/components/parts/note-list/NoteList.js
@@ -3,11 +3,8 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
 
 export default function NoteList() {
   return (
@@ -16,18 +13,12 @@ export default function NoteList() {
         <List>
           <ListItem disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
+              <ListItemText primary="Linux Reference" />
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
+              <ListItemText primary="k8s Notes" />
             </ListItemButton>
           </ListItem>
         </List>
