f9cf66bfa1a89ee2eda1df1cd878b8c560bb0db9
James Cathcart <47301496+james-cathcart@users.noreply.github.com>
2021-11-01 09:19:07 -0600

    Removed old duplication currentNote object from state

--- src/constants/InitialState.js
+++ src/constants/InitialState.js
@@ -14,12 +14,6 @@ const initialState = {
       lastViewed: "2021-09-24T16:00:00",
     },
   ],
-  currentNote: {
-    id: "mockNote1ID",
-    body: "<h1>Mock Linux References</h1>\n<p>Do stuff with the command line!!! Here's how to get started...</p>",
-    created: "2021-09-27T12:00:00",
-    lastViewed: "2021-09-27T14:00:00",
-  },
   currentNoteIndex: 0,
 };
 
